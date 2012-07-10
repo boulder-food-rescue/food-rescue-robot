ac01c9930efaef42529ccf8104e3039707c24dba
Caleb Phillips <cphillips@smallwhitecube.com>
2012-07-10 11:56:04 -0600

    fix small bug dealing with nil food type, remove myself from CC and update admin email

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,12 +1,12 @@
 class Notifier < ActionMailer::Base
-  AdminEmail = "cphillips@smallwhitecube.com"
+  AdminEmail = "info@boulderfoodrescue.org"
   default :from => "robot@boulderfoodrescue.org"
 
   def volunteer_log_reminder(volunteer,logs)
     @logs = logs
     @volunteer = volunteer
     # commented out for debugging purposes
-    mail(:to => volunteer.email, :cc => AdminEmail, :subject => "BFR Data Entry Reminder"){ |format| format.text }
+    mail(:to => volunteer.email, :subject => "BFR Data Entry Reminder"){ |format| format.text }
     #mail(:to => AdminEmail, :subject => "BFR Data Entry Reminder"){ |format| format.text }
   end
 

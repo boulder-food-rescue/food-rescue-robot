c9879604e129e1684accf65e96f0acc72c372ee2
Caleb Phillips <cphillips@smallwhitecube.com>
2012-07-17 20:57:47 -0600

    add home page with statistics

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -23,11 +23,14 @@ class VolunteersController < ApplicationController
     conf.list.sorting = {:name => 'ASC'}
     conf.columns = [:name,:email,:phone,:preferred_contact,:gone_until,:has_car,:is_disabled, 
                     :admin,:on_email_list,:pickup_prefs,:transport_type,:admin_notes,:regions,:created_at]
+    conf.update.columns = [:name,:email,:phone,:preferred_contact,:gone_until,:has_car,:is_disabled,
+                    :admin,:on_email_list,:pickup_prefs,:transport_type,:admin_notes,:regions,:password]
     conf.columns[:is_disabled].label = "Account Deactivated"
     conf.columns[:preferred_contact].form_ui = :select
     conf.columns[:regions].form_ui = :select
     conf.columns[:transport_type].form_ui = :select
     conf.columns[:preferred_contact].options = {:options => [["Email","Email"],["Phone","Phone"],["Text","Text"]]}
+    conf.actions.exclude :create
   end
 
   # Custom views of the index table
@@ -41,6 +44,42 @@ class VolunteersController < ApplicationController
   end
   def conditions_for_collection
     @conditions
+  end
+
+  # Other views entirely
+  def home
+    if current_volunteer.assignments.length == 0
+      @unassigned = true
+      @base_conditions = nil
+    else
+      @unassigned = false
+      @base_conditions = " AND region_id IN (#{current_volunteer.assignments.collect{ |a| a.region_id }.join(",")})"
+    end
+    @me = current_volunteer
+    @pickups = Log.where("volunteer_id = ? AND weight IS NOT NULL",current_volunteer.id)
+    @lbs = 0.0
+    @human_pct = 0.0
+    @num_pickups = {}
+    @num_covered = 0
+    @biggest = nil
+    @earliest = nil
+    @bike = TransportType.where("name = 'Bike'").shift
+    @pickups.each{ |l|
+      l.transport_type = @bike if l.transport_type.nil?
+      @num_pickups[l.transport_type] = 0 if @num_pickups[l.transport_type].nil?
+      @num_pickups[l.transport_type] += 1
+      @num_covered += 1 if l.orig_volunteer != @me and !l.orig_volunteer.nil?
+      @lbs += l.weight
+      @biggest = l if @biggest.nil? or l.weight > @biggest.weight
+      @earliest = l if @earliest.nil? or l.when < @earliest.when
+    }
+    @human_pct = 100.0*@num_pickups.collect{ |t,c| t.name =~ /car/i ? nil : c }.compact.sum/@num_pickups.values.sum  
+    @num_shifts = Schedule.where("volunteer_id = ?",current_volunteer.id).count
+    @num_to_cover = Log.where("volunteer_id IS NULL#{@base_conditions}").count
+    @num_unassigned = Schedule.where("volunteer_id IS NULL AND donor_id IS NOT NULL and recipient_id IS NOT NULL#{@base_conditions}").count
+    respond_to do |format|
+      format.html # home.html.erb
+    end
   end
 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Webapp::Application.routes.draw do
   resources :volunteers do 
     as_routes 
     collection do
+      get :home
       get :unassigned
       get :shiftless
     end
@@ -103,7 +104,7 @@ Webapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'schedules#mine'
+  root :to => 'volunteers#home'
 
   # See how all your routes lay out with "rake routes"
 

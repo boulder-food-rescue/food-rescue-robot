9d688cb9a79929e34ed6f43ecba7a4dfa9e1ffd1
Rahul Bhargava <rahulb@media.mit.edu>
2013-03-14 14:15:23 -0400

    fixes migration to be db-neutral (was failing on mysql2)

--- db/migrate/20121030144508_volunteers_add_sncs_boolean.rb
+++ db/migrate/20121030144508_volunteers_add_sncs_boolean.rb
@@ -1,6 +1,6 @@
 class VolunteersAddSncsBoolean < ActiveRecord::Migration
   def up
-    add_column :volunteers, :get_sncs_email, :boolean, :null => false, :default => 'f'
+    add_column :volunteers, :get_sncs_email, :boolean, :null => false, :default => false
   end
 
   def down

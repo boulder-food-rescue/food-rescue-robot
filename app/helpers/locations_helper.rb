aba7e919634d216de3079ecf5cfc480cf0c5aea3
Rahul Bhargava <rahulb@media.mit.edu>
2013-04-25 16:02:35 -0400

    print detailed hours in human readable format (fixes #2)

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -8,4 +8,20 @@ module LocationsHelper
     end
   end
 
+  def readable_detailed_hours location
+    str = ''
+    (0..6).each do |index|
+      if location.open_on_day? index
+        str += Date::DAYNAMES[index] + ' from ' + format_time(location.read_attribute('day'+index.to_s+'_start')) + 
+          ' to ' + format_time(location.read_attribute('day'+index.to_s+'_end')) + '<br />'
+      end
+    end
+    str.html_safe
+  end
+
+  private 
+    def format_time t
+      I18n.l t, :format => "%I:%M%p",  :locale => :"en"
+    end
+
 end

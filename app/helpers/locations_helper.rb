d9fec28d55ab089aa775bf6ee2801e8596632f6c
Rahul Bhargava <rahulb@media.mit.edu>
2013-04-26 11:14:52 -0400

    hours on location details page (fixes #4)

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -8,18 +8,33 @@ module LocationsHelper
     end
   end
 
-  def readable_detailed_hours location
-    str = ''
-    (0..6).each do |index|
-      if location.open_on_day? index
-        str += Date::DAYNAMES[index] + ' from ' + format_time(location.read_attribute('day'+index.to_s+'_start')) + 
-          ' to ' + format_time(location.read_attribute('day'+index.to_s+'_end')) + '<br />'
-      end
+  def readable_hours location
+    str = ""
+    if Webapp::Application.config.use_detailed_hours 
+      str = readable_detailed_hours location
+    else
+      str = readable_simple_hours location
     end
-    str.html_safe
+    str
   end
 
   private 
+
+    def readable_simple_hours loc
+      loc.hours.gsub("\n","<br>").html_safe unless loc.hours.nil? 
+    end
+
+    def readable_detailed_hours location
+      str = ''
+      (0..6).each do |index|
+        if location.open_on_day? index
+          str += Date::DAYNAMES[index] + ' from ' + format_time(location.read_attribute('day'+index.to_s+'_start')) + 
+            ' to ' + format_time(location.read_attribute('day'+index.to_s+'_end')) + '<br />'
+        end
+      end
+      str.html_safe
+    end
+
     def format_time t
       I18n.l t, :format => "%I:%M%p",  :locale => :"en"
     end

c9bbff348fb57d77d4976eabe36e2810d6faa8bb
Caleb Phillips <caleb@virgil.smallwhitecube.com>
2013-06-20 16:08:20 -0600

    Regions is function again, and without active scaffolding

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,5 +1,47 @@
 class RegionsController < ApplicationController
   before_filter :authenticate_volunteer!, :except => [:recipients, :request_rescue]
+  before_filter :super_admin_only, :except => [:recipients, :request_rescue]
+
+  def super_admin_only
+    redirect_to(root_path) unless current_volunteer.super_admin?
+  end
+
+  def index
+    @regions = Region.all
+    render :index
+  end
+
+  def edit
+    @region = Region.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @region = Region.find(params[:id])
+    if @region.update_attributes(params[:region])
+      flash[:notice] = "Updated Successfully."
+      index
+    else
+      flash[:notice] = "Update failed :("
+      render :edit
+    end
+  end
+
+  def new
+    @region = Region.new
+    render :new
+  end
+
+  def create
+    @region = Region.new(params[:region])
+    if @region.save
+      flash[:notice] = "Created successfully."
+      index
+    else
+      flash[:notice] = "Didn't save successfully :("
+      render :new
+    end
+  end
 
   def recipients
     @region = Region.find(params[:id])
@@ -26,5 +68,12 @@ class RegionsController < ApplicationController
       end
     end
   end
+
+  def destroy
+    @r = Region.find(params[:id])
+    @r.destroy
+    index
+  end
+
 
 end 

d4cf0c218ca544abebabc7c10c20619ab67e0e26
Caleb Phillips <caleb@virgil.smallwhitecube.com>
2013-06-20 11:58:27 -0600

    Moved to Rahul's responsive style for all but the login page. Necessitated some changes to JS stuff. Broke region administration (for super admins) in the process. Still to be fixed...

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,24 +1,6 @@
 class RegionsController < ApplicationController
   before_filter :authenticate_volunteer!, :except => [:recipients, :request_rescue]
 
-  active_scaffold :region do |conf|
-    conf.columns = [:logo,:name,:title,:tagline,:address,:phone,:tax_id,:lat,:lng,:notes,:website,:time_zone]
-    conf.update.columns = [:logo,:name,:title,:tagline,:address,:phone,:tax_id,:lat,:lng,:notes,:website,:handbook_url,:prior_lbs_rescued,
-                           :prior_num_pickups,:twitter_key,:twitter_secret,:twitter_token,:twitter_token_secret,:welcome_email_text,:splash_html,
-                           :time_zone, :weight_units]
-    # if marking isn't enabled it creates errors on delete :(
-    conf.actions.add :mark
-  end
-  def create_authorized?
-    current_volunteer.super_admin?
-  end
-  def update_authorized?(record=nil)
-    current_volunteer.super_admin?
-  end
-  def delete_authorized?(record=nil)
-    current_volunteer.super_admin?
-  end
-
   def recipients
     @region = Region.find(params[:id])
     @locations = Location.recipients.where(:region_id=>@region.id)
@@ -29,7 +11,6 @@ class RegionsController < ApplicationController
         "width" =>  '32', "height" => '37'
       })
     end
-    render :layout => 'responsive'
   end
 
   def request_rescue
@@ -44,7 +25,6 @@ class RegionsController < ApplicationController
         end
       end
     end
-    render :layout => 'responsive'
   end
 
 end 

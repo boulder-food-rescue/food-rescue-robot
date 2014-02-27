f1b6f1e883c03a93c12e27060d7d046bddff8184
Zach Doyle <zedoyle@gmail.com>
2014-02-27 13:07:12 -0700

    Clipping out redundant scale initialization

--- db/migrate/20140207200738_create_scale_types.rb
+++ db/migrate/20140207200738_create_scale_types.rb
@@ -9,16 +9,6 @@ class CreateScaleTypes < ActiveRecord::Migration
 		change_table :logs do |t|
       t.references :scale_type
     end
-  	Region.all.each{ |r|
-    	ScaleType.where("region_id IS NULL").each{ |st|
-      	stnew = ScaleType.new
-      	stnew.region_id = r.id
-      	stnew.name = st.name
-      	stnew.save
-      	execute "UPDATE schedule_parts SET scale_type_id=#{stnew.id} FROM schedules s WHERE scale_type_id=#{st.id} AND s.id=schedule_id AND s.region_id=#{r.id};"
-      	execute "UPDATE log_parts SET scale_type_id=#{stnew.id} FROM logs l WHERE scale_type_id=#{st.id} AND l.id=log_id AND l.region_id=#{r.id};"
-      }
-    }
 		Region.all.each{ |r|
     	defaultName = "Bathroom Scale (default)"
     	check = ScaleType.where('name = ?',defaultName)

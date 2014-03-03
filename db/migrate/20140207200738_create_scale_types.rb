76e02153c06ffda79473c4e7cbd7938b91676c78
Zach Doyle <zedoyle@gmail.com>
2014-03-03 13:17:09 -0700

    indentation fixes, migrate changes

--- db/migrate/20140207200738_create_scale_types.rb
+++ db/migrate/20140207200738_create_scale_types.rb
@@ -6,28 +6,28 @@ class CreateScaleTypes < ActiveRecord::Migration
       t.timestamps
       t.references :region
     end
-		change_table :logs do |t|
+    change_table :logs do |t|
       t.references :scale_type
     end
-		Region.all.each{ |r|
-    	defaultName = "Bathroom Scale (default)"
-    	check = ScaleType.where('name = ?',defaultName)
-    	check2 = ScaleType.where('region_id = ?',r.id)
-    	unless check.length >= 1
-      	t = ScaleType.new
-      	t.name = defaultName
-      	t.weight_unit = "lb"
-      	t.region_id=r.id
-      	t.save
-    	end
+    Region.all.each{ |r|
+      defaultName = "Bathroom Scale (default)"
+      check = ScaleType.where('name = ?',defaultName)
+      check2 = ScaleType.where('region_id = ?',r.id)
+      unless check.length >= 1
+        t = ScaleType.new
+        t.name = defaultName
+        t.weight_unit = "lb"
+        t.region_id=r.id
+        t.save
+      end
     }
   end
 
-	def down
-		execute "DELETE FROM scale_types WHERE region_id IS NOT NULL"
+  def down
+    execute "DELETE FROM scale_types WHERE region_id IS NOT NULL"
     remove_column :scale_types, :region_id
-		change_table :logs do |t|
+    change_table :logs do |t|
       t.remove :scale_type
     end
-		drop_table :scale_types
-	end
+    drop_table :scale_types
+  end

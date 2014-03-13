9c48916abe70c145be5712462fd58693c595c382
Zach Doyle <zedoyle@gmail.com>
2014-03-13 01:05:38 -0600

    added check to conversion

--- app/models/log_part.rb
+++ app/models/log_part.rb
@@ -8,11 +8,13 @@ class LogPart < ActiveRecord::Base
       if(self.log.region.scale_types.length==1)
         self.log.scale_type_id = self.log.region.scale_types.first.id
       end
-      scale = ScaleType.where('id = ?',self.log.scale_type_id)
-	  weight_unit = scale.first.weight_unit
-	  conv_weight = self.weight.to_f
-	  conv_weight = (conv_weight * (1/2.2).to_f) if weight_unit == "kg"
-  	  conv_weight = (conv_weight * (1/14).to_f) if weight_unit == "st"
-	  self.weight = conv_weight
+      if scale_type_id.changed? 
+        scale = ScaleType.where('id = ?',self.log.scale_type_id)
+	    weight_unit = scale.first.weight_unit
+  	    conv_weight = self.weight.to_f
+  	    conv_weight = (conv_weight * (1/2.2).to_f) if weight_unit == "kg"
+  	    conv_weight = (conv_weight * (1/14).to_f) if weight_unit == "st"
+	    self.weight = conv_weight
+      end
   }
 end

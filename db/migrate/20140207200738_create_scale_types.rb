3155f5dbcc346e9f0575787a87cb77d0f07d9b9b
Zach Doyle <zedoyle@gmail.com>
2014-03-16 13:28:37 -0600

    Scale migration reflects old scales

--- db/migrate/20140207200738_create_scale_types.rb
+++ db/migrate/20140207200738_create_scale_types.rb
@@ -9,21 +9,33 @@ class CreateScaleTypes < ActiveRecord::Migration
     change_table :logs do |t|
       t.references :scale_type
     end
+    old_scales = Log.select("scale_type").where("region_id = ?",r).collect{ |l| l.scale_type }.uniq
+    old_scales.each{ |s|
+      sn = ScaleType.new
+      sn.region_id = r.id
+      sn.name = Log::ScaleTypes[s]
+      sn.unit = "lb"
+      sn.save
+      Log.where("region_id = ? and scale_type = ?",r.id,s).each{ |ol|
+        ol.scale_type_id = sn.id
+        ol.save
+      }
+    }
     Region.all.each{ |r|
-      defaultName = "Bathroom Scale [default]"
-      check = ScaleType.where('name = ?',defaultName)
-      check = check.where('region_id = ?',r.id)
-      unless check.length >= 1
+      #defaultName = "Bathroom Scale [default]"
+      #check = ScaleType.where('name = ?',defaultName)
+      #check = check.where('region_id = ?',r.id)
+      unless ScaleType.where('region_id = ?',r.id).length >= 1
         t = ScaleType.new
         t.name = defaultName
         t.weight_unit = "lb"
         t.region_id=r.id
         t.save
-      end
-      defaultName = "Guesstimate [default]"
-      check = ScaleType.where('name = ?',defaultName)
-      check = check.where('region_id = ?',r.id)
-      unless check.length >= 1
+      #end
+      #defaultName = "Guesstimate [default]"
+      #check = ScaleType.where('name = ?',defaultName)
+      #check = check.where('region_id = ?',r.id)
+      #unless check.length >= 1
         t = ScaleType.new
         t.name = defaultName
         t.weight_unit = "lb"

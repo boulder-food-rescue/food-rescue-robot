6ffb81b268b8ac967501a5bd93a72d64be8b72f9
caleb <caleb@zeropercent.us>
2014-04-08 20:09:13 -0700

    problems with HUD

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,9 +6,9 @@ class LocationsController < ApplicationController
     if (params[:key] == @loc.receipt_key) or current_volunteer.region_admin?(@loc.region) or current_volunteer.super_admin?
       @schedules = @loc.is_donor ? Schedule.where("donor_id = ?",@loc.id) : Schedule.where("recipient_id = ?",@loc.id)
       if @loc.is_donor
-        @logs = Log.joins(:food_types).select("sum(weight) as weight_sum, string_agg(food_types.name,', ') as food_types_combined, logs.id, logs.transport_type_id, logs.when, logs.volunteer_id").where("donor_id = ?",@loc.id).group("logs.id, logs.transport_type_id, logs.when, logs.volunteer_id").order("logs.when ASC")
+        @logs = Log.joins(:food_types).select("sum(weight) as weight_sum, string_agg(food_types.name,', ') as food_types_combined, logs.id, logs.transport_type_id, logs.when").where("donor_id = ?",@loc.id).group("logs.id, logs.transport_type_id, logs.when").order("logs.when ASC")
       else 
-        @logs = Log.joins(:food_types).select("sum(weight) as weight_sum, string_agg(food_types.name,', ') as food_types_combined, logs.donor_id, logs.id, logs.transport_type_id, logs.when, logs.volunteer_id").where("recipient_id = ?",@loc.id).group("logs.donor_id, logs.id, logs.transport_type_id, logs.when, logs.volunteer_id").order("logs.when ASC")
+        @logs = Log.joins(:food_types).select("sum(weight) as weight_sum, string_agg(food_types.name,', ') as food_types_combined, logs.donor_id, logs.id, logs.transport_type_id, logs.when").where("recipient_id = ?",@loc.id).group("logs.donor_id, logs.id, logs.transport_type_id, logs.when").order("logs.when ASC")
       end
       render :hud
     else

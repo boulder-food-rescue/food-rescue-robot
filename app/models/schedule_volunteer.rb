894af0eb8736dc2910865b97411e6b2e8df47dbe
Zach Doyle <zedoyle@gmail.com>
2014-04-22 21:24:39 -0600

    schedule_volunteer belongs_to schedule_chain

--- app/models/schedule_volunteer.rb
+++ app/models/schedule_volunteer.rb
@@ -1,9 +1,9 @@
 class ScheduleVolunteer < ActiveRecord::Base
 	
-	belongs_to :schedule
+	belongs_to :schedule_chain
 	belongs_to :volunteer
   
-  attr_accessible :schedule_id, :volunteer_id, :active
+  attr_accessible :schedule_chain_id, :volunteer_id, :active
 
 	accepts_nested_attributes_for :volunteer	
 

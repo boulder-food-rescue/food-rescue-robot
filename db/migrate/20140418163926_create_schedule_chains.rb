b39c18f4c7674870f0069887d25c77eeb7e06704
Zach Doyle <zedoyle@gmail.com>
2014-04-22 18:07:23 -0600

    more basic migrate changes

--- db/migrate/20140418163926_create_schedule_chains.rb
+++ db/migrate/20140418163926_create_schedule_chains.rb
@@ -1,11 +1,26 @@
 class CreateScheduleChains < ActiveRecord::Migration
   def up
 		create_table :schedule_chains do |c|
-			t.references :volunteer
-			t.references :schedule
+			c.references :volunteer
+			c.references :schedule
+			c.time :start_time
+			c.time :stop_time
+			c.date :chain_date
 		end
-		Schedule.all.each{ |s|
-			
+		change_table :schedules do |s|
+			s.references :schedule_chain
+			s.references :location
+		end
+		Schedule.all.each{ |donor|
+			sc = ScheduleChain.new
+			recip = donor
+			recip.schedule_chain_id = sc.id
+			donor.schedule_chain_id = sc.id
+			recip.location_id = donor.recipient_id
+			donor.location_id = donor.donor_id
+			sc.save
+			recip.save
+			donor.save
 		}
   end
 

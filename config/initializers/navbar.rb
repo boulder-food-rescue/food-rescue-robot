d91dfab9040bdbfe7951c9e97239a552260be5f0
Rylan Bowers <rylanb@gmail.com>
2016-05-29 12:45:48 -0600

    Updating application layout whitespace and to use new navbar gem helpers. Updating version of bootstrap to 3.

--- config/initializers/navbar.rb
+++ config/initializers/navbar.rb
@@ -1,3 +1,6 @@
 BootstrapNavbar.configure do |config|
-  config.bootstrap_version = '2.8.8'
+  config.bootstrap_version = '3.2.0.2'
+  config.current_url_method = 'request.original_url'
 end
+
+ActionView::Base.send :include, BootstrapNavbar::Helpers

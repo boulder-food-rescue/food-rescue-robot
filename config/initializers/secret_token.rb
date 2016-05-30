627caf4122d3a6343ad459b5415e678317608ee0
Rylan Bowers <rylanb@gmail.com>
2016-05-29 18:30:51 -0600

    Setting secret token not secret_key_base in secret_token.rb.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,5 +1,5 @@
 if Rails.env.development?
-  Webapp::Application.config.secret_key_base = 'dev_0aac6asdfalsdfjkalsdfkdlsfljkasljkdflkajsflkjsdlkjflksdfjdsklf'
+  Webapp::Application.config.secret_token = 'dev_0aac6asdfalsdfjkalsdfkdlsfljkasljkdflkajsflkjsdlkjflksdfjdsklf'
 else
   Webapp::Application.config.secret_token = ENV['SECRET_KEY_BASE']
 end

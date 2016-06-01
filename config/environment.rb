33bbc6abc5fbfdd134ee876ab4ef31a305b0477f
Rylan Bowers <rylanb@gmail.com>
2016-05-31 18:22:02 -0600

    Moving the exception notification back to the production rb file. For Rails 3 its not really clear how its supposed to work.

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Webapp::Application.initialize!
-
-#Exception Notification
-Webapp::Application.config.middleware.use ExceptionNotifier,
-  :email => {
-    :email_prefix => "[BFR ROBOT ERROR] ",
-    :sender_address => %{"BFR Robot" <notifier@boulderfoodrescue.org>},
-    :exception_recipients => %w{rylanb@gmail.com cphillips@smallwhitecube.com}
-  }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,9 @@ Webapp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.middleware.use ExceptionNotifier,
+    :email_prefix => "[BFR ROBOT ERROR] ",
+    :sender_address => %{"BFR Robot" <notifier@boulderfoodrescue.org>},
+    :exception_recipients => %w{rylanb@gmail.com cphillips@smallwhitecube.com}
 end

010446d1c5cda4c91d1128881522db89346e450f
Rylan Bowers <rylanb@gmail.com>
2016-06-03 09:55:54 -0600

    Code cleanup in mailers for newer syntax and spacing / indentation.

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,6 +1,6 @@
 class Notifier < ActionMailer::Base
   add_template_helper(ApplicationHelper)
-  default :from => "robot@boulderfoodrescue.org"
+  default from: "robot@boulderfoodrescue.org"
   #ForceTo = "cphillips@smallwhitecube.com"
   ForceTo = nil
 
@@ -13,63 +13,63 @@ class Notifier < ActionMailer::Base
     @shifts = shifts
     to = admin_emails_for_region(@schedule.region)
     to = ForceTo.nil? ? to : ForceTo
-    mail(:to => to, :subject => "[FoodRobot] Schedule Collision Warning"){ |format| format.html }
+    mail(to: to, subject: "[FoodRobot] Schedule Collision Warning"){ |format| format.html }
   end
 
-  def region_welcome_email(region,volunteer)
+  def region_welcome_email(region, volunteer)
     return nil if region.welcome_email_text.nil? or region.welcome_email_text.strip.length == 0
     @welcome_email_text = region.welcome_email_text
     to = ForceTo.nil? ? volunteer.email : ForceTo
-    mail(:to => to, :subject => "[FoodRobot] Welcome to the Food Rescue Robot!"){ |format| format.html }
-  end  
+    mail(to: to, subject: "[FoodRobot] Welcome to the Food Rescue Robot!"){ |format| format.html }
+  end
 
-  def volunteer_log_reminder(volunteer,logs)
+  def volunteer_log_reminder(volunteer, logs)
     @logs = logs
     @volunteer = volunteer
     to = ForceTo.nil? ? volunteer.email : ForceTo
-    mail(:to => to, :subject => "[FoodRobot] Reminder: How much food did you pick up!?"){ |format| format.html }
+    mail(to: to, subject: "[FoodRobot] Reminder: How much food did you pick up!?"){ |format| format.html }
   end
 
-  def volunteer_log_pre_reminder(volunteer,logs)
+  def volunteer_log_pre_reminder(volunteer, logs)
     @logs = logs
     @logs = logs
     @volunteer = volunteer
     to = ForceTo.nil? ? volunteer.email : ForceTo
-    mail(:to => to, :subject => "[FoodRobot] Upcoming Pick-up Reminder"){ |format| format.html }
+    mail(to: to, subject: "[FoodRobot] Upcoming Pick-up Reminder"){ |format| format.html }
   end
 
-  def volunteer_log_sms_reminder(volunteer,logs)
+  def volunteer_log_sms_reminder(volunteer, logs)
     @logs = logs
     @volunteer = volunteer
     return nil if volunteer.nil?
     return nil if volunteer.sms_email.nil?
     to = ForceTo.nil? ? volunteer.sms_email : ForceTo
-    mail(:to => to, :subject => "[Food Robot]"){ |format| format.text }
+    mail(to: to, subject: "[Food Robot]"){ |format| format.text }
   end
 
-  def volunteer_log_sms_pre_reminder(volunteer,logs)
+  def volunteer_log_sms_pre_reminder(volunteer, logs)
     @logs = logs
     @volunteer = volunteer
     return nil if volunteer.nil?
     return nil if volunteer.sms_email.nil?
     to = ForceTo.nil? ? volunteer.sms_email : ForceTo
-    mail(:to => to, :subject => "[FoodRobot]"){ |format| format.text }
+    mail(to: to, subject: "[FoodRobot]"){ |format| format.text }
   end
 
   def admin_reminder_summary(region,logs)
     @logs = logs
     to = admin_emails_for_region(region)
-    to = ForceTo.nil? ? to : ForceTo 
-    mail(:to => to, :subject => "[FoodRobot] #{region.name} Data Entry Reminder Summary"){ |format| format.html }
+    to = ForceTo.nil? ? to : ForceTo
+    mail(to: to, subject: "[FoodRobot] #{region.name} Data Entry Reminder Summary"){ |format| format.html }
   end
 
   def admin_short_term_cover_summary(region,logs)
     @logs = logs
-    to = admin_emails_for_region(region) + Volunteer.where("get_sncs_email").collect{ |v| 
+    to = admin_emails_for_region(region) + Volunteer.where("get_sncs_email").collect{ |v|
       (v.region_ids.include?(region.id)) ? v.email : nil
     }.compact
     to = ForceTo.nil? ? to : ForceTo
-    mail(:to => to, :subject => "[FoodRobot] #{region.name} Shifts Needing Coverage Soon (SNCS!)"){ |format| format.html }
+    mail(to: to, subject: "[FoodRobot] #{region.name} Shifts Needing Coverage Soon (SNCS!)"){ |format| format.html }
   end
 
   def admin_weekly_summary(region,lbs,flagged_logs,biggest,num_logs,num_entered,zero_logs)
@@ -80,8 +80,8 @@ class Notifier < ActionMailer::Base
     @num_logs = num_logs
     @num_entered = num_entered
     @zero_logs = zero_logs
-    to = admin_emails_for_region(region) 
+    to = admin_emails_for_region(region)
     to = ForceTo.nil? ? to : ForceTo
-    mail(:to => to, :subject => "[FoodRobot] #{region.name} Weekly Summary"){ |format| format.html }
+    mail(to: to, subject: "[FoodRobot] #{region.name} Weekly Summary"){ |format| format.html }
   end
 end

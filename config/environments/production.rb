697babbf7fbfacb5760dc549dac3693d877ae705
Rylan Bowers <rylanb@gmail.com>
2016-07-13 23:01:31 -0600

    Updating to remove // from the mailer default url as force_ssl is on. Silly.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Webapp::Application.configure do
   }
   # ActionMailer Config
   #config.action_mailer.default_url_options = { :host => 'robot.boulderfoodrescue.org' }
-  config.action_mailer.default_url_options = { host: "//#{ENV['DOMAIN_NAME']}" }
+  config.action_mailer.default_url_options = { host: "#{ENV['DOMAIN_NAME']}" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true

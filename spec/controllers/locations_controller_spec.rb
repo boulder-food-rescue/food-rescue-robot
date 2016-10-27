dfdef7185945726c1c53a093b5c162bbeac823f8
Timothy Clayton <timothy.daniel.clayton@gmail.com>
2016-10-26 19:54:29 -0600

    adds basic controller specs

---
+++ spec/controllers/locations_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe LocationsController do
+  let(:volunteer) { create :volunteer_with_assignment }
+
+  it_behaves_like 'an authenticated indexable resource'
+end

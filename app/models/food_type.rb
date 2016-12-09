e8efe694ce643853817b44aa3b9c1ab70b1c52b9
Ryan Foster <ryan@mojotech.com>
2016-12-09 13:20:32 -0700

    Clean up FoodType
    - add blank spec file

--- app/models/food_type.rb
+++ app/models/food_type.rb
@@ -1,13 +1,14 @@
 class FoodType < ActiveRecord::Base
   attr_accessible :name, :region_id
-  has_many :schedules, :through => :schedule_parts
-  has_many :schedule_parts
-  has_many :log_parts
-  has_many :logs, :through => :log_parts
-  belongs_to :region
-  default_scope { where(active:true) }
 
-  def self.regional(region_id)
-    where(:region_id=>region_id)
-  end
+  belongs_to :region
+
+  has_many :schedule_parts
+  has_many :schedules, through: :schedule_parts
+  has_many :log_parts
+  has_many :logs, through: :log_parts
+
+  default_scope { where(active: true) }
+
+  scope :regional -> (region_id) { where(region_id: region_id) }
 end

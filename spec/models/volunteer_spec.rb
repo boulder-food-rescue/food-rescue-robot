551c1af0299109e8be56094528d2179260c91f6d
Eric Freese <ericdfreese@gmail.com>
2016-12-22 16:29:20 -0700

    Add test for Volunteer#needs_training?

--- spec/models/volunteer_spec.rb
+++ spec/models/volunteer_spec.rb
@@ -16,4 +16,36 @@ RSpec.describe Volunteer do
       expect(subject.unassigned?).to eq(false)
     end
   end
+
+  describe '#needs_training?' do
+    subject { volunteer }
+
+    context 'when the volunteer has no completed logs' do
+      let(:log_volunteer) { create(:log_volunteer, volunteer: subject) }
+
+      before do
+        log_volunteer.log.complete = false
+        log_volunteer.log.save!
+      end
+
+      it 'returns true' do
+        expect(subject.needs_training?).to eq(true)
+      end
+    end
+
+    context 'when the volunteer has a completed log' do
+      let(:log_volunteer) { create(:log_volunteer, volunteer: subject) }
+
+      before do
+        log_volunteer.log.complete = true
+        log_volunteer.log.hours_spent = 1
+        log_volunteer.log.why_zero = Log::WhyZero.invert["No Food"]
+        log_volunteer.log.save!
+      end
+
+      it 'returns false' do
+        expect(subject.needs_training?).to eq(false)
+      end
+    end
+  end
 end

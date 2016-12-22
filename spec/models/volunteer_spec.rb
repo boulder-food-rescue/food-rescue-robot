772e50db8779220c334309f6b4d957c7b3be7f7b
Eric Freese <ericdfreese@gmail.com>
2016-12-22 16:28:45 -0700

    Add test for Volunteer#unassigned?

--- spec/models/volunteer_spec.rb
+++ spec/models/volunteer_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Volunteer do
   let(:volunteer) { create(:volunteer) }
+
+  describe '#unassigned?' do
+    subject { volunteer }
+
+    it 'returns true if the volunteer has no assignments' do
+      subject.assignments.destroy_all
+      expect(subject.unassigned?).to eq(true)
+    end
+
+    it 'returns false if the volunteer has any assignments' do
+      create(:assignment, volunteer: subject)
+      expect(subject.unassigned?).to eq(false)
+    end
+  end
 end

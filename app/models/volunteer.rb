007a389c1e4d6328fa59f30efdd90a1e44f62176
Eric Freese <ericdfreese@gmail.com>
2016-12-22 22:25:53 -0700

    Implement Volunteer::inactive with SQL

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -169,12 +169,14 @@ class Volunteer < ActiveRecord::Base
     end
   end
 
-  # TODO: turn this into SQL
   def self.inactive(region_ids = nil)
-    find_all_by_active(false).
-      keep_if do |v|
-        (region_ids.nil? || (v.region_ids & region_ids).length > 0)
-      end
+    query = where(active: false)
+
+    if region_ids.present?
+      query.joins(:regions).where(regions: { id: region_ids }).group('volunteers.id')
+    else
+      query
+    end
   end
 
   def self.all_for_region(region_id)

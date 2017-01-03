8468be2ae8bba6f937d8008026d771753b4dcf94
Eric Freese <ericdfreese@gmail.com>
2017-01-03 13:08:17 -0700

    Fix Schedule factory

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -1,14 +1,14 @@
 FactoryGirl.define do
   factory :schedule do
     position 3
-    location (Location.all.count >= 5 ? Location.all.sort_by{ rand }.first : create(:location))
+    location { Location.all.count >= 5 ? Location.all.sort_by{ rand }.first : create(:location) }
 
     factory :donation_schedule do
-      location (Location.donors.count >= 5 ? Location.donors.sort_by{ rand }.first : create(:donor))
+      location { Location.donors.count >= 5 ? Location.donors.sort_by{ rand }.first : create(:donor) }
     end
 
     factory :recipient_schedule do
-      location (Location.recipients.count >= 5 ? Location.recipients.sort_by{ rand }.first : create(:recipient))
+      location { Location.recipients.count >= 5 ? Location.recipients.sort_by{ rand }.first : create(:recipient) }
     end
   end
 end

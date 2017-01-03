0b2e7dc0967f652d0f897a450f0300add9cbfed0
Eric Freese <ericdfreese@gmail.com>
2017-01-03 13:38:14 -0700

    Fix ScheduleChain factory
    Schedule#weekly? is defined as frequency == 'weekly', which does a case-
    sensitive comparison. Schedule#frequency is expected to be lowercase.

--- spec/factories/schedule_chains.rb
+++ spec/factories/schedule_chains.rb
@@ -10,7 +10,7 @@ FactoryGirl.define do
     public_notes "some other notes"
     expected_weight { rand(200) }
     day_of_week { rand(6) }
-    frequency "Weekly"
+    frequency "weekly"
     difficulty_rating { rand(3) }
     hilliness { rand(3) }
   end

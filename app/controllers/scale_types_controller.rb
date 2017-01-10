706f0ffdb0d08414e66da7673244af64170000c7
Eric Freese <ericdfreese@gmail.com>
2017-01-10 16:30:41 -0700

    Add ScaleType pundit policy

--- app/controllers/scale_types_controller.rb
+++ app/controllers/scale_types_controller.rb
@@ -2,8 +2,8 @@ class ScaleTypesController < ApplicationController
   before_filter :authenticate_volunteer!
 
   def index
-    region_ids = current_volunteer.region_ids
-    @scale_types = ScaleType.where("region_id IN (#{region_ids.join(",")})")
+    authorize FoodType
+    @scale_types = policy_scope(ScaleType)
     respond_to do |format|
       format.json { render json: @scale_types.to_json }
       format.html { render :index }
@@ -12,7 +12,7 @@ class ScaleTypesController < ApplicationController
 
   def destroy
     @l = ScaleType.find(params[:id])
-    return unless check_permissions(@l)
+    authorize @l
     @l.active = false
     @l.save
     redirect_to(request.referrer)
@@ -23,14 +23,14 @@ class ScaleTypesController < ApplicationController
     @scale_type.region_id = params[:region_id]
     @scale_type.weight_unit = 1
     @action = "create"
-    return unless check_permissions(@scale_type)
+    authorize @scale_type
     session[:my_return_to] = request.referrer
     render :new
   end
 
   def create
     @scale_type = ScaleType.new(params[:scale_type])
-    return unless check_permissions(@scale_type)
+    authorize @scale_type
     if @scale_type.save
       flash[:notice] = "Created successfully."
       unless session[:my_return_to].nil?
@@ -46,7 +46,7 @@ class ScaleTypesController < ApplicationController
 
   def edit
     @scale_type = ScaleType.find(params[:id])
-    return unless check_permissions(@scale_type)
+    authorize @scale_type
     @action = "update"
     session[:my_return_to] = request.referrer
     render :edit
@@ -54,7 +54,7 @@ class ScaleTypesController < ApplicationController
 
   def update
     @scale_type = ScaleType.find(params[:id])
-    return unless check_permissions(@scale_type)
+    authorize @scale_type
     if @scale_type.update_attributes(params[:scale_type])
       flash[:notice] = "Updated successfully."
       unless session[:my_return_to].nil?
@@ -66,14 +66,5 @@ class ScaleTypesController < ApplicationController
       flash[:notice] = "Scale update didn't go through."
       render :edit
     end
-  end
-
-  def check_permissions(l)
-    unless current_volunteer.super_admin? or (current_volunteer.admin_region_ids.include? l.region_id) 
-      flash[:notice] = "Unauthorized to edit scales in that way."
-      redirect_to(root_path)
-      return false
-    end
-    return true
   end
 end
---
+++ app/policies/scale_type_policy.rb
@@ -0,0 +1,51 @@
+class ScaleTypePolicy < ApplicationPolicy
+  def index?
+    super_admin? || region_admin?
+  end
+
+  def create?
+    super_admin? || region_admin_of?(record.region_id)
+  end
+
+  def update?
+    super_admin? || region_admin_of?(record.region_id)
+  end
+
+  def destroy?
+    super_admin? || region_admin_of?(record.region_id)
+  end
+
+  private
+
+  def super_admin?
+    volunteer.admin?
+  end
+
+  def region_admin?
+    !volunteer.assignments.where(admin: true).empty?
+  end
+
+  def admin_region_ids
+    volunteer.assignments.where(admin: true).pluck(:region_id)
+  end
+
+  def region_admin_of?(*region_ids)
+    (region_ids - admin_region_ids).empty?
+  end
+
+  class Scope < Scope
+    def resolve
+      if volunteer.admin?
+        scope.all
+      else
+        scope.where(region_id: admin_region_ids)
+      end
+    end
+
+    private
+
+    def admin_region_ids
+      volunteer.assignments.where(admin: true).pluck(:region_id)
+    end
+  end
+end

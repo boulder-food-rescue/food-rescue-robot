51c2d31f2c7fbc79be80b070b187540a40399bed
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-07 06:45:41 -0700

    Remove unnecessary coments from absences_controller_spec while focusing on feature_spec development

--- spec/controllers/absences_controller_spec.rb
+++ spec/controllers/absences_controller_spec.rb
@@ -1,13 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe AbsencesController do
-  # let(:volunteer_with_assignment) { create :volunteer_with_assignment }
-  # let(:volunteer_without_assignment) { create :volunteer }
-  # let(:admin) { create :volunteer_with_assignment, admin: true }
+  let(:volunteer_with_assignment) { create :volunteer_with_assignment }
 
-  # it_behaves_like 'an authenticated indexable resource'
+  it_behaves_like 'an authenticated indexable resource'
 
-  # describe 'GET #create' do
-
-  # end
 end

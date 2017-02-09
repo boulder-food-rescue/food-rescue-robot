8ba44dc110b446cf292cd5b9434591511ea33610
Traci7822 <Tracir@gmail.com>
2017-02-08 17:25:10 -0700

    added recipient factory to spec

--- spec/factories/log_recipients.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :log_recipient do
-    association :log
-    association :recipient
-  end
-end
--- spec/features/take_one_time_shift.rb
+++ spec/features/take_one_time_shift.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Volunteer takes One-Time Shifts" do
-  let(:log) { create(:log, when: "Tue, 31 Jan 2017", donor: donor, log_recipients: [LogRecipient.create(name: "Recipient Name")]) }
+  let(:log) { create(:log, when: "Tue, 31 Jan 2017", donor: donor, recipients: [recipient]) }
   let(:donor) { create(:donor, name: "This is a name") }
   let(:volunteer) { create(:volunteer, :not_waived, regions: [log.region], assigned: true) }
   let(:location) { create(:location) }
-
+  let(:recipient) { create(:recipient, name: "Recipient")}
   # Expect page to show log entry
   feature "An available one-time shift" do
     before do
       login volunteer
       location
-      binding.pry
+      recipient
     end
 
     it "can view an available shift" do

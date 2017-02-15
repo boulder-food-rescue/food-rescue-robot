13017fe257ed525315eff494b6b7d57b1d11d078
Douglas Lovell <doug@wbreeze.com>
2017-02-15 14:28:01 -0500

    Limit the unique index of log-volunteers to those with active = true

--- db/migrate/20170126022713_prevent_duplicate_log_volunteers.rb
+++ db/migrate/20170126022713_prevent_duplicate_log_volunteers.rb
@@ -1,10 +1,15 @@
 require File.join(__dir__, '../../lib/de_dup_log_volunteers.rb')
 class PreventDuplicateLogVolunteers < ActiveRecord::Migration
+  INDEX_NAME = 'index_log_volunteers_on_log_id_and_volunteer_id'
   def up
     DeDupLogVolunteers.de_duplicate
-    add_index :log_volunteers, [:log_id, :volunteer_id], unique: true
+    ActiveRecord::Base.connection.execute <<-SQL
+      CREATE UNIQUE INDEX "#{INDEX_NAME}"
+      ON "log_volunteers" ("log_id", "volunteer_id")
+      WHERE active is true
+    SQL
   end
   def down
-    remove_index :log_volunteers, column: [:log_id, :volunteer_id]
+    remove_index :log_volunteers, name: INDEX_NAME
   end
 end

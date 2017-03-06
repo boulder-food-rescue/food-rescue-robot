f189e9f6768723fc7f6963374b2f1752e806b551
moveson <mark.oveson@gmail.com>
2017-03-06 09:22:52 -0700

    Remove LogPart#scale_weight method (not used in code and appears to be faulty).

--- app/models/log_part.rb
+++ app/models/log_part.rb
@@ -2,17 +2,4 @@ class LogPart < ActiveRecord::Base
   belongs_to :log
   belongs_to :food_type
   attr_accessible :required, :weight, :count, :description, :food_type_id, :log_id
-
-  # weight in db is always lbs, so convert to what the user expects to see (in the units of the scale)
-  def scale_weight
-    display_unit = self.log.scale_type.weight_unit
-    if display_unit == 'kg'
-      self.weight*2.2
-    elsif display_unit == 'st'
-      self.weight*14
-    else
-      self.weight
-    end
-  end
-
 end

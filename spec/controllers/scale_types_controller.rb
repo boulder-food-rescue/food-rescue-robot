f89f6f17781029e67f28edd2e340e9fa865cac1e
dania02525 <dania02525@gmail.com>
2017-04-26 20:20:56 -0600

    set up controller spec files

---
+++ spec/controllers/scale_types_controller.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe ScaleTypesController do
+
+  it 'asserts the truth' do
+    expect(true).to be_truthy
+  end
+end
---
+++ spec/controllers/waivers_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe WaiversController do
+
+  it 'asserts the truth' do
+    puts "\n #{self.class} needs specs"
+    expect(true).to be_truthy
+  end
+end

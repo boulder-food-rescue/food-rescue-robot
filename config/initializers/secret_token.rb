580b16d136a44a647d6c33e36625b0286cf25681
moveson <mark.oveson@gmail.com>
2017-04-26 20:26:52 -0600

    Add dotenv-rails gem and related files; replace hardcoded keys with environment variable references; add .env.example file with instructions for use.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,5 +1,1 @@
-Webapp::Application.config.secret_token = if Rails.env.production?
-                                            ENV['SECRET_KEY_BASE']
-                                          else
-                                            'dev_0aac6asdfalsdfjkalsdfkdlsfljkasljkdflkajsflkjsdlkjflksdfjdsklf'
-                                          end
+Webapp::Application.config.secret_token = ENV['SECRET_KEY_BASE']

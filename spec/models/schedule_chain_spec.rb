4e4bfcedbcf1f5fdffc816f6519ed0cbb528af0d
Noel Worden <noelworden@gmail.com>
2017-04-28 08:16:47 -0600

    All Models now have specs

---
+++ spec/models/schedule_chain_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe ScheduleChain do
+  describe 'this file needs to be completed' do
+    let(:super_admin) { create(:volunteer, admin: true) }
+
+    xit 'validates super_admin' do
+      expect(super_admin).to be_valid
+    end
+  end
+end

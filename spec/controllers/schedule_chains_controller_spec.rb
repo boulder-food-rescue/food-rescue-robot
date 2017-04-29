1a00125741eb1276c977709a57ab65710b4a0602
dania02525 <dania02525@gmail.com>
2017-04-29 06:59:28 -0600

    format specs better

--- spec/controllers/schedule_chains_controller_spec.rb
+++ spec/controllers/schedule_chains_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ScheduleChainsController do
 
   it 'asserts the truth' do
-    puts "#{self.class} needs specs"
+    puts "\n #{self.class} needs specs"
     expect(true).to be_truthy
   end
 end

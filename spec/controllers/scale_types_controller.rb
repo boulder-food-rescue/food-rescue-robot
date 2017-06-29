19099325a5ab13d27f9a6a7eb0bdb1458a7e17cc
Nathanael Beisiegel <pknb.dev@gmail.com>
2017-06-28 19:33:00 -0600

    Autofixed some leading whitespace before tacking rubocop file

--- spec/controllers/scale_types_controller.rb
+++ spec/controllers/scale_types_controller.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ScaleTypesController do
-
   xit 'asserts the truth' do
     expect(true).to be_truthy
   end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController do
-
   xit 'asserts the truth' do
     expect(true).to be_truthy
   end

8728b010f8bd0427f5ddece2a0e549ff6643fbbd
Eddie <glen0071@gmail.com>
2018-03-05 12:32:16 -0600

    remove paperclip reference

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,15 +93,4 @@ Webapp::Application.configure do
 
   ExceptionNotifier::Rake.configure
 
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION')
-    },
-    url: ':s3_domain_url',
-    path: '/:class/:attachment/:id_partition/:style/:filename'
-  }
-
 end

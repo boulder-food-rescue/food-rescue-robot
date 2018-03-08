ed12f7ac346667f8e25bd208e4d5efca7d5f466c
Traci Thompson <Tracir@gmail.com>
2018-03-08 12:16:07 -0700

    testing take one shift log view (#81)
    * testing take one shift log view
    
    * donor name test added and passing, recipient in progress
    
    * added recipient factory to spec
    
    * log.recipients fixed
    
    * Updating to get take_one_time_shift to pass

--- spec/factories/logs.rb
+++ spec/factories/logs.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :log do
-    notes 'testing 123'
+    notes 'Log Notes Testing123'
     weight_unit 'lb'
     region { (Region.all.count >= 5 ? Region.all.sort_by{ rand }.first : create(:region)) }
     self.when { Date.today + (rand > 0.5 ? -1 : 1)*rand(10) } # have to do self. since when is a reserved word in ruby

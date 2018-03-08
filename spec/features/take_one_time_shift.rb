d270420f95ecef7a51c3b941f54c085100b229d2
Rylan Bowers <rylanb@gmail.com>
2018-03-08 12:13:36 -0700

    Updating to get take_one_time_shift to pass

--- spec/features/take_one_time_shift.rb
+++ spec/features/take_one_time_shift.rb
@@ -1,35 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "Volunteer takes One-Time Shifts" do
-  let(:log) { create(:log, when: "Tue, 31 Jan 2017", donor: donor, recipients: [recipient]) }
   let(:donor) { create(:donor, name: "This is a name") }
   let(:volunteer) { create(:volunteer, :not_waived, regions: [log.region], assigned: true) }
-  let(:location) { create(:location) }
-  let(:recipient) { create(:recipient)}
+  let!(:location) { create(:location) }
+  let!(:recipient) { create(:recipient, name: 'Donor Recipient Name')}
+  let(:log_date) { 3.days.from_now }
+  let(:log) { create(:log, when: log_date, donor: donor, recipients: [recipient]) }
+
   # Expect page to show log entry
-  feature "An available one-time shift" do
+  feature 'An available one-time shift' do
     before do
       login volunteer
-      location
-      recipient
     end
 
-    it "can view an available shift" do
-      visit "/logs/open"
+    it 'can view an available shift' do
+      visit '/logs/open'
       # Log notes
-      expect(page).to have_content("testing 123")
+      expect(page).to have_content('Log Notes Testing123')
       # When
-      expect(page).to have_content("Tue Jan 31")
+      expect(page).to have_content(log_date.strftime("%a %b %e"))
       # Volunteer name
-      expect(page).to have_content("John Doe")
+      expect(page).to have_content('John Doe')
       # Region tagline
-      expect(page).to have_content("Knowing is Half the Battle")
+      expect(page).to have_content('Knowing is Half the Battle')
       # Recipient name
-      expect(page).to have_content("Some place")
+      expect(page).to have_content('Some place')
       # Donor name
-      expect(page).to have_content("This is a name")
+      expect(page).to have_content('This is a name')
     end
   end
 end
-
-#Expect cover shift link to _____

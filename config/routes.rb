609af6e7326a8c0d91ee67abb100f61946a04918
eddie_g <glen0071@gmail.com>
2018-03-20 11:49:25 -0500

    Revert "split out functions from region_admin view"

--- config/routes.rb
+++ config/routes.rb
@@ -40,8 +40,6 @@ Webapp::Application.routes.draw do
       get :stats_service
       get :stats
       get :export
-      get :new_export_data
-      get :new_receipt
     end
     member do
       get :take
@@ -112,7 +110,6 @@ Webapp::Application.routes.draw do
       get :create
       get :destroy
       get :assign
-      get :new_switch_user
     end
     member do
       get :reactivate

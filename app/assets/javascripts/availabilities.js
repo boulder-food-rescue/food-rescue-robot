e25eaf636533d6cb1f8a989326219538817d9a58
Eddie <glen0071@gmail.com>
2018-04-03 19:27:02 -0500

    finish availabilities viewing js

--- app/assets/javascripts/availabilities.js
+++ app/assets/javascripts/availabilities.js
@@ -1,44 +1,16 @@
-// $("#table_of_volunteers").css("display", "none");
-$(document).ready(function(){
-    $("#submit").click(function(){
-        $("#table_of_volunteers").toggle();
-    });
+$(".availability").css("display", "none");
+
+$('#days').change(function() {
+  toggleAvailabilities()
 });
 
-$(document).ready(function(){
-  $('#days').change(function() {
-    var day = $("#days option:selected").val();
-    alert(day);
-  });
+$('#times').change(function() {
+  toggleAvailabilities()
 });
 
-$(document).ready(function(){
-    $('#times').change(function() {
-    var time = $("#times option:selected").val();
-      alert(time);
-  });
-});
-
-// Submit button listener
-  // $("#submit_button").click(function() {
-  //   var day = $('#dropdown_day').val();
-  //   var time = $('#dropdown_day').val();
-  //
-  //
-  // })
-
-//$(document).ready(function(){
-  //$("button").click(function() {
-    //$("button").removeClass('disabled');
-//  });
-//});
-//console.log('test');
-
-// $('button#monday').onChange(function(e) {
-
-
-
-//$('button#monday').click(function(e) {
-
-  // search for li elements with relevant data
-  // .toggle();
+function toggleAvailabilities(){
+  var day = $("#days option:selected").val();
+  var time = $("#times option:selected").val();
+  $(".availability").css("display", "none");
+  $('.' + day + '.' + time + '').css("display", "inline");
+}

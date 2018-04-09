6615f17eb0a0ea5557eb10cfad7006c0c12be0ae
Eloa Verona <efrancav@macalester.edu>
2018-04-09 11:05:53 -0500

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -52,27 +52,6 @@ ActiveRecord::Schema.define(:version => 20180404161115) do
     t.string "format"
   end
 
-  create_table "donors", :force => true do |t|
-    t.string   "email",                  :default => "",  :null => false
-    t.string   "encrypted_password",     :default => "",  :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0,   :null => false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                              :null => false
-    t.datetime "updated_at",                              :null => false
-    t.string   "name",                   :default => "f"
-    t.string   "phone"
-    t.integer  "region_id"
-  end
-
-  add_index "donors", ["email"], :name => "index_donors_on_email", :unique => true
-  add_index "donors", ["reset_password_token"], :name => "index_donors_on_reset_password_token", :unique => true
-
   create_table "food_types", :force => true do |t|
     t.string   "name"
     t.datetime "created_at",                   :null => false

68383211169c4a221ebf76abcbaece6c532e745d
Rylan Bowers <rylanb@gmail.com>
2018-04-25 18:41:06 -0600

    Fixing small rubocop errors in volunteers_controller / log_volunteer.rb

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -50,7 +50,7 @@ class VolunteersController < ApplicationController
   end
 
   def need_training
-    @volunteers =  Volunteer.needing_training(current_volunteer.region_ids)
+    @volunteers = Volunteer.needing_training(current_volunteer.region_ids)
     @header = 'Volunteers Needing Training'
     render :index
   end
--- app/models/log_volunteer.rb
+++ app/models/log_volunteer.rb
@@ -9,6 +9,7 @@ class LogVolunteer < ActiveRecord::Base
   validate :prevent_active_duplicates
 
   private
+
   def prevent_active_duplicates
     existing_active = self.class.where(log_id: log_id, volunteer_id: volunteer_id, active: true)
     errors.add(:base, "volunteer #{volunteer_id} is already assigned to log #{log_id}") if active && existing_active.any?

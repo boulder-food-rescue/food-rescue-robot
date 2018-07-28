820a80112c0c1de9b79e58b58c6ca65d2643e907
Rylan Bowers <rylanb@gmail.com>
2018-07-28 09:42:54 -0600

    Updating spelling

--- app/models/log.rb
+++ app/models/log.rb
@@ -207,7 +207,7 @@ class Log < ActiveRecord::Base
                 lps.collect{ |lp| lp.weight }.join(':'),
                 lps.collect{ |lp| lp.description.nil? ? 'None' : lp.description }.join(':'),
                 log.summed_weight, log.donor.nil? ? 'Unknown' : log.donor.name, log.recipients.collect{ |r| r.nil? ? 'Unknown' : r.name }.join(':'),
-                log.volunteers.collect{ |r| r.nil? ? 'Unknown' : r.name }.join(':'), log.scale_type.nil? ? 'Uknown' : log.scale_type.name,
+                log.volunteers.collect{ |r| r.nil? ? 'Unknown' : r.name }.join(':'), log.scale_type.nil? ? 'Unknown' : log.scale_type.name,
                 log.transport_type.nil? ? 'Unknown' : log.transport_type.name, log.hours_spent, log.num_reminders, log.notes]
       end
     end

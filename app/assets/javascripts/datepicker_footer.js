8d13c9ada97dbdf76b04be368cb1e155dd308c3e
Brandon Baker <brandonbaker40@icloud.com>
2018-10-26 15:11:52 -0700

    moves datepicker in footer to separate file

---
+++ app/assets/javascripts/datepicker_footer.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  console.log("datepicker laodaed");
+  flatpickr(".datepicker", {
+    "dateFormat":"Y-n-j",
+    "allowInput":false,
+    "onOpen": function(selectedDates, dateStr, instance) {
+      instance.setDate(instance.input.value, false);
+    }
+  });
+});

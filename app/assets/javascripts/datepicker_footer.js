bdfd59c4d3b58d63ad9c9cc00e7c06dff601bdcb
Rylan Bowers <rylanb@gmail.com>
2018-12-29 11:46:21 -0700

    Cleaning up datepicker to work properly in a production ENV w/o extraneous header / footer.

--- app/assets/javascripts/datepicker_footer.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  flatpickr(".datepicker", {
-    "dateFormat": "Y-n-j",
-    "allowInput": false,
-    "onOpen": function(selectedDates, dateStr, instance) {
-      instance.setDate(instance.input.value, false);
-    }
-  });
-});

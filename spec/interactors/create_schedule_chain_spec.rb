33308dd0fed77458579262c43e02e7db40b2f066
Rylan Bowers <rylanb@gmail.com>
2019-02-22 21:34:27 -0700

    Fixing to get tests to pass at all times of the day. Seems later at night that they don’t pass.

--- spec/interactors/create_schedule_chain_spec.rb
+++ spec/interactors/create_schedule_chain_spec.rb
@@ -4,15 +4,9 @@ require 'rails_helper'
 
 RSpec.describe CreateScheduleChain do
   describe '::call' do
-    subject do
-      described_class.call(
-        schedule_chain: chain
-      )
-    end
+    subject { described_class.call( schedule_chain: chain ) }
 
-    let(:chain) do
-      build(:schedule_chain, :one_time)
-    end
+    let(:chain) { build(:schedule_chain, :one_time) }
 
     it 'saves the schedule' do
       expect(subject.success?).to eq(true)
@@ -20,9 +14,7 @@ RSpec.describe CreateScheduleChain do
     end
 
     it 'does not create rescue logs' do
-      expect {
-        subject
-      }.to_not(change { Log.count })
+      expect { subject }.to_not(change { Log.count })
     end
 
     it 'fails if it cannot save the schedule' do
@@ -33,15 +25,10 @@ RSpec.describe CreateScheduleChain do
     context 'schedule is one time and same day' do
       let!(:donor) { create(:donation_schedule, schedule_chain: chain, location: create(:hub), position: 1) }
       let!(:recipient) { create(:recipient_schedule, schedule_chain: chain, location: create(:hub), position: 2) }
-
-      let(:chain) do
-        create(:schedule_chain, :one_time, detailed_date: Date.today)
-      end
+      let(:chain) { create(:schedule_chain, :one_time, detailed_date: Date.current) }
 
       it 'creates rescue logs' do
-        expect {
-          subject
-        }.to change { Log.count }.by(1)
+        expect { subject }.to change { Log.count }.by(1)
       end
     end
   end

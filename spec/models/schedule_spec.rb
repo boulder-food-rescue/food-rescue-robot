aa8fba1e64262aa637e51f9d1a6d2ace387578fe
Rylan Bowers <rylanb@gmail.com>
2019-02-24 11:12:26 -0700

    Adding schedule spec start.

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -3,10 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Schedule do
-  describe 'this file needs to be completed' do
+  describe 'Schedule' do
     let(:super_admin) { create(:volunteer, admin: true) }
 
-    xit 'validates super_admin' do
+    it 'validates super_admin' do
       expect(super_admin).to be_valid
     end
   end
